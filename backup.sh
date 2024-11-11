b65ee722cebfb623144e198e82624f7d6ee5b486
KNPOB <qilong-kirov.liu@outlook.com>
2024-11-11 17:06:04 +0800

    backup apple notes

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@ drive_path='/Volumes/BACKUP'
 local_path='/Users/knpob/Territory'
 icloud_path='/Users/knpob/Documents'
 app_path='/Users/knpob/Library/Mobile Documents'
+apple_notes_path='/Users/knpob/Library/Group Containers/group.com.apple.notes'
 
 # colors
 green='\033[0;32m'
@@ -34,3 +35,10 @@ do
     echo "backup $green$src$nocolor >> $green$des$nocolor"
     rsync -av --delete --exclude '*-nosync' "$src" "$des"
 done
+
+# backup apple notes
+# p.s. Grant Full Disk Access to VS Code's terminal:
+# for macOS: System Preferences > Privacy & Security > Full Disk Access, enable Visual Studio Code
+des="$drive_path/App/group.com.apple.notes"
+echo "backup $green$apple_notes_path$nocolor >> $green$des$nocolor"
+rsync -av --delete --exclude '*-nosync' "$apple_notes_path" "$des"
