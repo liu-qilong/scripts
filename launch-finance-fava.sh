90e45b29324492b0a07437ddd0c719c2fa6f2de7
KNPOB <qilong-kirov.liu@outlook.com>
2024-12-25 12:10:22 +0800

    scripts for launching beancount fava

---
+++ launch-finance-fava.sh
@@ -0,0 +1,2 @@
+cd "/Users/knpob/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/beancount"
+fava finance.beancount -p 5060
---
+++ launch-time-fava.sh
@@ -0,0 +1,2 @@
+cd "/Users/knpob/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/beancount"
+fava time.beancount -p 5050
