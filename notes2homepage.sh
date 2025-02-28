56e7ecacd9c38fa278328ff11bfe965bf0378ddc
KNPOB <qilong-kirov.liu@outlook.com>
2025-02-28 11:31:42 +0800

    fix

--- notes2homepage.sh
+++ notes2homepage.sh
@@ -1,5 +1,5 @@
 notes_path='/Users/knpob/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes'
-homepage_path='/Users/knpob/Territory/Marx/homepage'
+homepage_path='/Users/knpob/Territory/Weber/homepage'
 
 rsync -av --delete --exclude '*-private*' --exclude '.DS_Store' "$notes_path/blog" "$homepage_path/contents"
 rsync -av --delete --exclude '*-private*' --exclude '.DS_Store' "$notes_path/note" "$homepage_path/contents"
